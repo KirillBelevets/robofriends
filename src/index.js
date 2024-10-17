6b37f0d0969f0b533f07610f5f8b481d63cb7ddd
Kirill Belevets <kirillbelevets@Kirills-MacBook-Air.local>
2024-10-17 12:54:37 +0300

    add functionality

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
-// import ReactDOM from 'react-dom'; The new way to import createRoot:
+// import ReactDOM from 'react-dom'; // The new way to import createRoot:
+import { Provider, connect } from 'react-redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import { createRoot } from "react-dom/client";
+import { thunk } from 'redux-thunk';
 import './index.css'; 
 import App from './containers/App';
 import 'tachyons';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import { searchRobots, requestRobots } from "./reducers";
 
+const logger = createLogger();
 
+const rootReducers = combineReducers({searchRobots, requestRobots});
+const store = createStore(rootReducers, applyMiddleware(thunk, logger));
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
 
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();
